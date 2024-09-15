11c079eb9cb0c9a2c3d78ac503d7744f0258feaf
fosterchild1 <crannberriesa@gmail.com>
2024-09-15 21:01:46 +0300

    ye

--- SeedFinder/Class1.cs
+++ SeedFinder/Class1.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 using HarmonyLib.Tools;
 using SeedFinder;
+using SeedFinder.Patches;
 using UnityEngine;
+using UnityEngine.AI;
 using static System.Random;
 
 namespace SeedFinder
@@ -22,39 +28,6 @@ namespace SeedFinder
         public static SeedFinderBase Instance;
         internal ManualLogSource logger;
 
-        public class LevelScrap
-        {
-            public int rarity { get; set; }
-            public bool twoHanded { get; set; }
-            public string name { get; set; }
-        }
-
-        private List<LevelScrap> ExperimentationScrap = new List<LevelScrap>
-        {
-            new LevelScrap{rarity=80, twoHanded=true, name="Large axle"},
-            new LevelScrap{rarity=90, twoHanded=true, name="V-type engine"},
-            new LevelScrap{rarity=12, twoHanded=false, name="Plastic fish"},
-            new LevelScrap{rarity=88, twoHanded=false, name="Metal sheet"},
-            new LevelScrap{rarity=4, twoHanded=false, name="Laser pointer"},
-            new LevelScrap{rarity=88, twoHanded=false, name="Big bolt"},
-            new LevelScrap{rarity=19, twoHanded=true, name="Bottles"},
-            new LevelScrap{rarity=3, twoHanded=false, name="Ring"},
-            new LevelScrap{rarity=32, twoHanded=false, name="Steering wheel"},
-            new LevelScrap{rarity=5, twoHanded=false, name="Cookie mold pan"},
-            new LevelScrap{rarity=10, twoHanded=false, name="Egg Beater"},
-            new LevelScrap{rarity=10, twoHanded=false, name="Jar of pickles"},
-            new LevelScrap{rarity=32, twoHanded=false, name="Dust pan"},
-            new LevelScrap{rarity=3, twoHanded=false, name="Airhorn"},
-            new LevelScrap{rarity=3, twoHanded=false, name="Clown horn"},
-            new LevelScrap{rarity=3, twoHanded=true, name="Cash register"},
-            new LevelScrap{rarity=2, twoHanded=false, name="Candy"},
-            new LevelScrap{rarity=1, twoHanded=false, name="Gold bar"},
-            new LevelScrap{rarity=6, twoHanded=false, name="Yield Sign"},
-            new LevelScrap{rarity=22, twoHanded=false, name="Homemade flashbang"},
-            new LevelScrap{rarity=17, twoHanded=false, name="Gift"},
-            new LevelScrap{rarity=42, twoHanded=false, name="Flask"},
-            new LevelScrap{rarity=5, twoHanded=false, name="Easter egg"}
-        };
 
         void Awake()
         {
@@ -65,39 +38,256 @@ namespace SeedFinder
             logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
 
             harmony.PatchAll(typeof(SeedFinderBase));
-
-            int targetItem = 16;
-
+            harmony.PatchAll(typeof(SeedFinderPatches));
             logger.LogInfo("finder awake");
-            for (int seed = 0; seed < 100000; seed++)
-            {
-              //  logger.LogInfo(seed.ToString());
-                System.Random AnomalyRandom = new System.Random(seed + 5);
-                int num = AnomalyRandom.Next(8, 12);
+        }
+    }
+}
 
-                if (AnomalyRandom.Next(0, 500) <= 25)
+namespace SeedFinder.Patches
+{
+    [HarmonyPatch(typeof(RoundManager))]
+    public class SeedFinderPatches
+    {
+        private static ManualLogSource logger = SeedFinderBase.Instance.logger;
+
+        private static SpawnableItemWithRarity CheckSSD(int seed, SelectableLevel level)
+        {
+            System.Random AnomalyRandom = new System.Random(seed + 5);
+            AnomalyRandom.Next(8, 12);
+
+            if (AnomalyRandom.Next(0, 500) <= 25)
+            {
+                //logger.LogInfo(string.Format("SSD Found: {0}", seed));
+                int num3 = AnomalyRandom.Next(0, level.spawnableScrap.Count);
+                bool flag = false;
+                for (int n = 0; n < 2; n++)
                 {
-                    //logger.LogInfo(string.Format("SSD Found: {0}", seed));
-                    int num3 = AnomalyRandom.Next(0, ExperimentationScrap.Count);
-                    bool flag = false;
-                    for (int n = 0; n < 2; n++)
+                    if (level.spawnableScrap[num3].rarity >= 5 && !level.spawnableScrap[num3].spawnableItem.twoHanded)
                     {
-                        if (ExperimentationScrap[num3].rarity >= 5 && !ExperimentationScrap[num3].twoHanded)
+                        flag = true;
+                        break;
+                    }
+                    num3 = AnomalyRandom.Next(0, level.spawnableScrap.Count);
+                }
+                if (!flag && AnomalyRandom.Next(0, 100) < 60)
+                {
+                    num3 = -1;
+                }
+                if (num3 != -1) 
+                {
+                    return level.spawnableScrap[num3];
+                }
+            }
+            return null;
+        }
+
+
+        private static Dictionary<string, int> getOutsideObjects(int seed, SelectableLevel currentLevel, NavMeshHit navHit, Transform[] shipSpawnPathPoints)
+        {
+            float RandomNumberInRadius(float radius, System.Random randomSeed)
+            {
+                return ((float)randomSeed.NextDouble() - 0.5f) * radius;
+            }
+            Vector3 GetRandomNavMeshPositionInBoxPredictable(Vector3 pos, float radius = 10f, NavMeshHit navHit_ = default(NavMeshHit), System.Random randomSeed = null, int layerMask = -1)
+            {
+                float y = pos.y;
+                float x = RandomNumberInRadius(radius, randomSeed);
+                float y2 = RandomNumberInRadius(radius, randomSeed);
+                float z = RandomNumberInRadius(radius, randomSeed);
+                Vector3 vector = new Vector3(x, y2, z) + pos;
+                vector.y = y;
+                float num = Vector3.Distance(pos, vector);
+                if (NavMesh.SamplePosition(vector, out navHit, num + 2f, layerMask))
+                {
+                    return navHit.position;
+                }
+                return pos;
+            }
+            Vector3 PositionEdgeCheck(Vector3 position, float width, NavMeshHit navHit_)
+            {
+                if (!NavMesh.FindClosestEdge(position, out navHit_, -1) || navHit_.distance >= width)
+                {
+                    return position;
+                }
+                Vector3 position2 = navHit_.position;
+                Ray ray = new Ray(position2, position - position2);
+                if (NavMesh.SamplePosition(ray.GetPoint(width + 0.5f), out navHit_, 10f, -1))
+                {
+                    position = navHit_.position;
+                    return position;
+                }
+                return Vector3.zero;
+            }
+
+
+            System.Random random = new System.Random(seed + 2);
+            GameObject[] outsideAINodes = (from x in GameObject.FindGameObjectsWithTag("OutsideAINode")
+                                   orderby Vector3.Distance(x.transform.position, Vector3.zero)
+                                   select x).ToArray<GameObject>();
+            NavMeshHit navMeshHit = default(NavMeshHit);
+
+            int num2 = 0;
+            List<Vector3> list = new List<Vector3>();
+            Dictionary<string, int> objectlist = new Dictionary<string, int>();
+            GameObject[] spawnDenialPoints = GameObject.FindGameObjectsWithTag("SpawnDenialPoint");
+            if (currentLevel.spawnableOutsideObjects != null)
+            {
+                for (int j = 0; j < currentLevel.spawnableOutsideObjects.Length; j++)
+                {
+                    var outsideobject = currentLevel.spawnableOutsideObjects[j];
+                    int width = outsideobject.spawnableObject.objectWidth;
+                    double num3 = random.NextDouble();
+                    int num = (int)outsideobject.randomAmount.Evaluate((float)num3);
+                    if (-1 == j)
+                    {
+                        num += 12;
+                    }
+                    if ((float)random.Next(0, 100) < 20f)
+                    {
+                        num *= 2;
+                    }
+                    int k = 0;
+                    while (k < num)
+                    {
+                        int num4 = random.Next(0, outsideAINodes.Length);
+                        Vector3 vector = GetRandomNavMeshPositionInBoxPredictable(outsideAINodes[num4].transform.position, 30f, navMeshHit, random, -1);
+                        if (outsideobject.spawnableObject.spawnableFloorTags == null)
                         {
-                            flag = true;
+                            goto IL_251;
+                        }
+                        bool flag = false;
+                        RaycastHit raycastHit;
+                        if (Physics.Raycast(vector + Vector3.up, Vector3.down, out raycastHit, 5f, StartOfRound.Instance.collidersAndRoomMaskAndDefault))
+                        {
+                            for (int l = 0; l < outsideobject.spawnableObject.spawnableFloorTags.Length; l++)
+                            {
+                                if (raycastHit.collider.transform.CompareTag(outsideobject.spawnableObject.spawnableFloorTags[l]))
+                                {
+                                    flag = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (flag)
+                        {
+                            goto IL_251;
+                        }
+                    IL_57F:
+                        k++;
+                        continue;
+                    IL_251:
+                        vector = PositionEdgeCheck(vector, (float)width, navHit);
+                        if (vector == Vector3.zero)
+                        {
+                            goto IL_57F;
+                        }
+                        bool flag2 = false;
+                        for (int m = 0; m < shipSpawnPathPoints.Length; m++)
+                        {
+                            if (Vector3.Distance(shipSpawnPathPoints[m].transform.position, vector) < (float)width + 6f)
+                            {
+                                flag2 = true;
+                                break;
+                            }
+                        }
+                        if (flag2)
+                        {
+                            goto IL_57F;
+                        }
+                        for (int n = 0; n < spawnDenialPoints.Length; n++)
+                        {
+                            if (Vector3.Distance(spawnDenialPoints[n].transform.position, vector) < (float)width + 6f)
+                            {
+                                flag2 = true;
+                                break;
+                            }
+                        }
+                        if (flag2)
+                        {
+                            goto IL_57F;
+                        }
+                        if (Vector3.Distance(GameObject.FindGameObjectWithTag("ItemShipLandingNode").transform.position, vector) < (float)width + 4f)
+                        {
                             break;
                         }
-                        num3 = AnomalyRandom.Next(0, ExperimentationScrap.Count);
-                    }
-                    if (!flag && AnomalyRandom.Next(0, 100) < 60)
-                    {
-                        num3 = -1;
-                    }
-                    if (num3 == targetItem)
-                    {
-                        logger.LogInfo(string.Format("SSD Found: {0}", seed));
+                        if (!flag2)
+                        {
+                            if (width > 4)
+                            {
+                                flag2 = false;
+                                for (int num5 = 0; num5 < list.Count; num5++)
+                                {
+                                    if (Vector3.Distance(vector, list[num5]) < (float)width)
+                                    {
+                                        flag2 = true;
+                                        break;
+                                    }
+                                }
+                                if (flag2)
+                                {
+                                    goto IL_57F;
+                                }
+                            }
+                            list.Add(vector);
+
+                            string objname = outsideobject.spawnableObject.name;
+                            if (!objectlist.ContainsKey(objname))
+                            {
+                                objectlist.Add(objname, 1);
+                            }
+                            objectlist[objname]++;
+                            num2++;
+                            goto IL_57F;
+                        }
+                        goto IL_57F;
                     }
                 }
+            }
+            return objectlist;
+        }
+
+        [HarmonyPatch("SpawnScrapInLevel")]
+        [HarmonyPostfix]
+        static void Patch(ref SelectableLevel ___currentLevel, ref NavMeshHit ___navHit, ref Transform[] ___shipSpawnPathPoints)
+        {
+            SelectableLevel level = ___currentLevel;
+            NavMeshHit navHit = ___navHit;
+            Transform[] shipSpawnPathPoints = ___shipSpawnPathPoints;
+
+
+            /*  foreach (var thing in level.spawnableScrap)
+              {
+                  logger.LogInfo(thing.spawnableItem.itemName);
+              }*/
+            Dictionary<string, int> outsideobjects = getOutsideObjects(StartOfRound.Instance.randomMapSeed, level, ___navHit, ___shipSpawnPathPoints);
+            foreach (KeyValuePair<string, int> kvp in outsideobjects)
+            {
+                logger.LogInfo(kvp.Key);
+                logger.LogInfo(kvp.Value);
+            }
+            int highestPumpkinAmount = 0;
+            void checkSeed(int seed)
+            {
+
+                //  logger.LogInfo(seed.ToString());
+                SpawnableItemWithRarity item = CheckSSD(seed, level);
+                Dictionary<string, int> outsideObjects = getOutsideObjects(seed, level, navHit, shipSpawnPathPoints);
+                if (item != null && item.spawnableItem.itemName == "Gold bar")
+                {
+                    logger.LogInfo(seed);
+                    int pumpkins;
+                    if (outsideObjects.TryGetValue("GiantPumpkin", out pumpkins) && pumpkins >= highestPumpkinAmount)
+                    {
+                        logger.LogInfo(string.Format("HIGH PUMPKIN AMOUNT ({0}): {1}", pumpkins, seed));
+                        highestPumpkinAmount = pumpkins;
+                    }
+                }
+            }
+           
+            for (int seed = 0; seed < 10000; seed++)
+            {
+                checkSeed(seed);
             }
         }
     }
